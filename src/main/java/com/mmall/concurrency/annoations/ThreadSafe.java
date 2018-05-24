6c5fbebe3a3813683a9d6efbfda06214c34b72d6
MrTomZhang <18838990559@163.com>
2018-05-25 00:08:00 +0800

    添加注解

---
+++ src/main/java/com/mmall/concurrency/annoations/ThreadSafe.java
@@ -0,0 +1,20 @@
+package com.mmall.concurrency.annoations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by 张洪涛 on 2018/5/24.
+ */
+
+
+/**
+ * 课程里用来标记【线程安全】的类
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface ThreadSafe {
+    String value() default "";
+}
